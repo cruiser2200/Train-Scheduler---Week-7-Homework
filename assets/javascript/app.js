59010b6035b49027187c1c9abbf35b5bdf4c10c4
Mike Smith <mike.smith1@earthlink.net>
2018-03-19 15:08:32 -0500

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDVzV2Kd-flf1POtBnm48M-c98X6GEBkSY",
+    authDomain: "train-scheduler-week-7-c0417.firebaseapp.com",
+    databaseURL: "https://train-scheduler-week-7-c0417.firebaseio.com",
+    projectId: "train-scheduler-week-7-c0417",
+    storageBucket: "train-scheduler-week-7-c0417.appspot.com",
+    messagingSenderId: "682076941787"
+  };
+  firebase.initializeApp(config);
+
+  
